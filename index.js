93ec2b9c5d682d91e8dafd72ad17cff326f5ce88
Cristel Capetillo <cristel.capetillo@outlook.com>
2022-05-16 10:18:45 +0200

    Fix for res.json and making kind a const

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const { Datastore } = require('@google-cloud/datastore');
 const datastore = new Datastore();
+const kind = 'customer';
 
 const cors = require('cors');
 app.use(cors());
@@ -12,26 +13,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/getCustomers', (req, res) => {
-    var query = datastore.createQuery('customer');
+    var query = datastore.createQuery(kind);
 
     datastore.runQuery(query, (err, data) => {
         if (err)
             console.log(err);
         else
-            var customerId = data.map((customer) => ({ id: customer[datastore.KEY].id, ...customer }));
-        res.send(customerId);
+            var customers = data.map((customer) => ({ id: customer[datastore.KEY].id, ...customer }));
+        res.json(customers);
     });
 });
 
 app.get('/getCustomer', (req, res) => {
     console.log(req.query);
-    const customerId = datastore.key(['customer', parseInt(req.query.customerId)]);
-    const query = datastore.createQuery('customer').filter('__key__', '=', customerId);
+    const customerId = datastore.key([kind, parseInt(req.query.customerId)]);
+    const query = datastore.createQuery(kind).filter('__key__', '=', customerId);
     datastore.runQuery(query, (err, data) => {
         if (err)
             console.log(err);
         else
-            res.send(data);
+            res.json(data[0]);
     });
 });
 
